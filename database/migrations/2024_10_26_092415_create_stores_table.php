197d48aa5ff18a43b451126667bc9f676044ee74
Mohammed Awda <awdadm4@gmail.com>
2024-11-17 08:29:24 +0200

    yum_gutt v1.2.1 new migration file for stores table

---
+++ database/migrations/2024_10_26_092415_create_stores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->json('description')->nullable();
+            $table->boolean('is_open')->default(0)->comment('this value override store schedule');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stores');
+    }
+};
